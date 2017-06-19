afff4565dfc2ee028df59b58969de672c5be29fa
Arka Sadhu <ark.sadhu2904@gmail.com>
2017-06-19 16:11:56 -0700

    Adding file_list

--- MediFor semantic integrity/code/protest_dataset/gen_all_bbx_ss.m
+++ MediFor semantic integrity/code/protest_dataset/gen_all_bbx_ss.m
@@ -2,10 +2,12 @@ img_tdir = '../../data/copied_data/';
 f = dir(img_tdir);
 f = f(3:end);
 bbx = cell(1, length(f));
+fls = cell(1, length(f));
 dict = containers.Map();
 for i=1:length(f)
     fname = fullfile(img_tdir, f(i).name);
     bbx{i} = gen_bbx_ss(imread(fname));
+    fls{i} = f(i).name;
     dict(f(i).name) = bbx{i};
     fprintf('Iter %d\n', i)
 end
