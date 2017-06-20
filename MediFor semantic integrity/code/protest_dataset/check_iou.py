dd8b3bf3ded600fa7fb28d2b415e67d7843e7f6c
TheShadow29 <ark.sadhu2904@gmail.com>
2017-06-20 16:07:28 -0700

    IOU check if only subset

--- MediFor semantic integrity/code/protest_dataset/check_iou.py
+++ MediFor semantic integrity/code/protest_dataset/check_iou.py
@@ -26,7 +26,7 @@ def get_iou(bbox_pred, bbox_gt):
     for the format of matplotlib imshow
     (x2, y2) being top right corner
     '''
-    
+
     x1_pred, y1_pred, x2_pred, y2_pred = bbox_pred
     x1_gt, y1_gt, x2_gt, y2_gt = bbox_gt
     intersection_coordinates = (max(x1_pred, x1_gt), max(y1_pred, y1_gt),
@@ -39,13 +39,42 @@ def get_iou(bbox_pred, bbox_gt):
     return area_overlap, area_union, float(area_overlap)/float(area_union)
 
 
+def get_iou2(bbox_pred, bbox_gt):
+    '''
+    Assumes the bbox is in the format:
+    x1, y1, x2, y2
+    (x1, y1) being bottom left corner
+    for the format of matplotlib imshow
+    (x2, y2) being top right corner
+    '''
+
+    x1_pred, y1_pred, x2_pred, y2_pred = bbox_pred
+    x1_gt, y1_gt, x2_gt, y2_gt = bbox_gt
+    intersection_coordinates = (max(x1_pred, x1_gt), max(y1_pred, y1_gt),
+                                min(x2_pred, x2_gt), min(y2_pred, y2_gt))
+
+    area_overlap = get_area(intersection_coordinates)
+    if area_overlap < 0:
+        # pdb.set_trace()
+        area_overlap = 0
+    area_union = get_area(bbox_pred) + get_area(bbox_gt) - area_overlap
+    if intersection_coordinates == (x1_gt, y1_gt, x2_gt, y2_gt):
+        return area_overlap, area_union, float(area_overlap)/float(area_union)
+    else:
+        iou = float(area_overlap)/float(area_union)
+        if iou > 0.9:
+            return area_overlap, area_union, iou
+        else:
+            return 0, area_union, 0
+
+        
 if __name__ == '__main__':
     img_tdir = '/arka_data/places_data/copied_data/'
     file_list = []
     for f in os.listdir(img_tdir):
         if f[-4:] == '.jpg' or f[-4:] == '.png':
             file_list.append(f)
-            
+
     gt_dict_bbox = pickle.load(open('../../data/protest_data/code_manip_bbox_dict.pkl', 'rb'))
     pred_dict_bbox = pickle.load(open('./code_manip_bbox_dict.pkl', 'rb'))
     total_num = 0
@@ -59,7 +88,7 @@ if __name__ == '__main__':
         for i in range(len(b2[:20])):
             b2[i][0], b2[i][1] = b2[i][1], b2[i][0]
             b2[i][2], b2[i][3] = b2[i][3], b2[i][2]
-            _, _, iou = get_iou(b2[i] - 1, b1)
+            _, _, iou = get_iou2(b2[i] - 1, b1)
             iou_list.append(iou)
         # print max(iou_list)
         iou_np = np.array(iou_list)
