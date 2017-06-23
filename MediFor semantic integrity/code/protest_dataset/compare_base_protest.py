58a1b0bdd7318de2ed0189f315d3f5c56a6455d4
TheShadow29 <ark.sadhu2904@gmail.com>
2017-06-23 12:41:46 -0700

    Very bad results from text lines, there may have been some bug

--- MediFor semantic integrity/code/protest_dataset/compare_base_protest.py
+++ MediFor semantic integrity/code/protest_dataset/compare_base_protest.py
@@ -17,22 +17,23 @@ from parse_all_data_nimble17 import nimble_references, nimble_prov_reference, pr
 import pdb
 import disp_img
 import imshow_coll
-
+from txt_data_storer import info_storer, info_storer_all
 
 
 
 if __name__ == '__main__':
     # probe_file = open('../../data/nimble17_data/alexnet365_dev3_seed_2.pkl', 'rb')
-    probe_file = open('../../data/protest_data/alexnet365_Modified_Images_ProtestL_txt_boxes.pkl', 'rb')
+    probe_file = open('../../data/protest_data/alexnet365_Modified_Images_ProtestL_\
+txt_boxes.pkl', 'rb')
+    # probe_file = open('../../data/protest_data/bvlc_alexnet_Modified_Images_ProtestL_\
+# txt_boxes.pkl', 'rb')
     probe_all_info = pickle.load(probe_file)
     # world_file = open('../../data/nimble17_data/alexnet365_NC2017_Dev3_Beta1_world.pkl')
-    world_file = open('../../data/protest_data/alexnet365_Pruned_Protest_YFCCImages_txt_boxes.pkl', 'rb')
+    world_file = open('../../data/protest_data/alexnet365_Pruned_Protest_\
+YFCCImages_txt_boxes.pkl', 'rb')
+    # world_file = open('../../data/protest_data/bvlc_alexnet_Pruned_Protest_\
+# YFCCImages_txt_boxes.pkl', 'rb')
     world_all_info = pickle.load(world_file)
-
-    num_corr = 0
-    total_num = 0
-    goods = 0
-    bad_nums = []
 
     # test_dir = '/mnt/disk1/ark_data/code_manip/dev3/seed_2/'
     test_dir = '/home/nkovvuri/Rama_Work/dataset/Protest_Images/Modified_Images_ProtestL/'
@@ -40,92 +41,46 @@ if __name__ == '__main__':
     for f in os.listdir(test_dir):
         if f[-4:] == '.jpg' or f[-4:] == '.png':
             test_imgs.append(f)
-    # for itern, p_node in enumerate(prov_data.nodes[:]):
+
     # wtdir = '/arka_data/NC2017_Dev1_Beta4/world/'
     # wtdir = '/mnt/disk1/ark_data/NC2017_Dev3_Beta1/NC2017_Dev3_Beta1/world/'
     wtdir = '/home/nkovvuri/Rama_Work/dataset/Protest_Images/Pruned_Protest_YFCCImages/'
-    for itern, pfid in enumerate(test_imgs):
+
+    probe_dict = dict()
+    world_dict = dict()
+    for ind, pdat in enumerate(probe_all_info.data):
+        probe_dict[pdat.fid] = ind
+    for ind, wdat in enumerate(world_all_info.data):
+        world_dict[wdat.fid] = ind
+
+    num_corr = 0
+    total_num = 0
+    goods = 0
+    bad_nums = []
+
+    for itern, pfid in enumerate(test_imgs[:]):
         # pdb.set_trace()
+        # base_gt_num = pfid.split.split('_')[-1]
         try:
-            print ("iter no. ", itern)
-            # p_info_idx = dict_probe[pfid]
-            probe_info = probe_all_info.data[probe_all_info.data_id_dict[pfid]]
-            fv_probe = probe_info.fv3
-            temp_array = np.array([])
-            id_array = np.array([])
-            # k = len(p_node.wfids)
-            k = 10
-            for idx, w in enumerate(world_all_info.data):
-                if pfid not in w.fid:
-                    # pdb.set_trace()
-                    # lol += 1
-                    corr = two_imgs_eff.cmp_fv(fv_probe, w.fv3, 'ncc')['pear_ncc']
-                    temp_array = np.append(temp_array, corr)
-                    id_array = np.append(id_array, idx)
-                    # else:
-                    # lol += 1
-            top_k = temp_array.argsort()[-k:]
-            top_k = list(reversed(top_k))
-            pritnt (emp_array[top_k])
-            ids = id_array[top_k].astype(int)
-            # pdb.set_trace()
-            base_gt = pfid.split('.')[0].split('_')[1]
-        # try:
-            wbase_gt_ind = world_all_info.data_id_dict[base_gt + '.png']
-        except KeyError as e:
-            wbase_gt_ind = world_all_info.data_id_dict[base_gt + '.jpg']
-        wbase_gt = world_all_info.data[wbase_gt_ind]
-        corr_gt = two_imgs_eff.cmp_fv(fv_probe, wbase_gt.fv3, 'ncc')['pear_ncc']
-        print ('Ground truth base corr ', corr_gt)
-        for i in range(k):
-            # if p_dat.base_browser_file_name in world_all_info.data[top_k[i]].fid:
-            guess = False
-            # if world_all_info.data[ids[i]].fid[:-4] in p_node.wfids:
-            # if world_all_info.data[ids[i]].fid.split('.')[0].split('/')[-1] in p_node.wfids:
-            wi = world_all_info.data[ids[i]]
-            bool1 = wi.fid.split('.')[0].split('/')[-1] == base_gt
-            corr1 = two_imgs_eff.cmp_fv(wi.fv3, wbase_gt.fv3, 'ncc')['pear_ncc']
-            bool2 = corr1 > 0.95
-            if bool1 or bool2:
-            # if bool1:
+            base_gt_num = pfid.split('_')[-1]
+            base_gt_file = 'protest_img_' + base_gt_num
+            p_info = probe_all_info.data[probe_dict[pfid]]
+            corr_arr = list()
+            m_cor_all_w = 0
+            wid_match = ''
+            for w_info in world_all_info.data:
+                for fvp in p_info.fv_list:
+                    for fvw in w_info.fv_list:
+                        corr = two_imgs_eff.cmp_fv(fvp, fvw, 'ncc')['pear_ncc']
+                        if corr > m_cor_all_w:
+                            m_r_all_w = corr
+                            wid_match = w_info.fid
+                            # pdb.set_trace()
+            if wid_match == base_gt_file:
                 num_corr += 1
-                guess = True
-                if (guess):
-                    if (temp_array[top_k[i]] > 0.95):
-                        goods += 1
-                # print
-                break
-            # if (guess):
-        # print
-        print ('Itern is ' + str(guess))
-        # guess = False
-        if not guess:
-        # if True:
-
-            bad_nums.append(itern)
-            print 'Pfid is ' + str(pfid)
-            tmp_w_array = [world_all_info.data[ids[i]] for i in range(k)]
-            print 'Wfids predicted are '
-            # wfids_to_print = ['world/' + str(w.fid) for w in tmp_w_array]
-            # pdb.set_trace()
-            wfids_to_print = [str(w.fid) for w in tmp_w_array]
-            # print [str(w.fid) for w in tmp_w_array]
-            print wfids_to_print
-            # l1 = '/mnt/disk1/ark_data/NC2017_Dev3_Beta1/NC2017_Dev3_Beta1/probe/' + str(pfid)
-            l1 = []
-            l1.append(test_dir + str(pfid))
-            # l1 = 'world/' + str(p_node.fid)
-            # pdb.set_trace()
-            # l1 = [l1] + [wtdir + base_gt]
-            l1.append(wtdir + base_gt)
-            wfids_to_print2 = [wtdir + w.fid for w in tmp_w_array]
-            l1 = l1 + wfids_to_print2
-            # l1.append(wfids_to_print)
-            # disp_img.show_img(l1)
-            # imshow_coll.imshow_collection_new(l1)
-            # pdb.set_trace()
-
-        total_num += 1
+            total_num += 1
+        except KeyError as e:
+            pass
 
     print (num_corr, total_num)
     print bad_nums
