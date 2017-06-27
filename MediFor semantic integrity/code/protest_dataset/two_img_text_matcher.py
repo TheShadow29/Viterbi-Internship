23d363c563565f17e3792118a5c98c2d1a310173
TheShadow29 <ark.sadhu2904@gmail.com>
2017-06-27 16:21:40 -0700

    Bad results using both nets for direct txt image matching

--- MediFor semantic integrity/code/protest_dataset/two_img_text_matcher.py
+++ MediFor semantic integrity/code/protest_dataset/two_img_text_matcher.py
@@ -6,13 +6,13 @@ import numpy as np
 
 
 if __name__ == '__main__':
-    # caffe_model_dir = '../../data/caffe_model/alexnet365/'
-    # descriptor_path = caffe_model_dir + 'deploy_alexnet_places365.prototxt'
-    # weights_path = caffe_model_dir + 'alexnet_places365.caffemodel'
+    caffe_model_dir = '../../data/caffe_model/alexnet365/'
+    descriptor_path = caffe_model_dir + 'deploy_alexnet_places365.prototxt'
+    weights_path = caffe_model_dir + 'alexnet_places365.caffemodel'
 
-    caffe_model_dir = '/home/arka_s/Caffe/caffe/models/bvlc_alexnet/'
-    descriptor_path = caffe_model_dir + 'deploy.prototxt'
-    weights_path = caffe_model_dir + 'bvlc_alexnet.caffemodel'
+    # caffe_model_dir = '/home/arka_s/Caffe/caffe/models/bvlc_alexnet/'
+    # descriptor_path = caffe_model_dir + 'deploy.prototxt'
+    # weights_path = caffe_model_dir + 'bvlc_alexnet.caffemodel'
 
     ilsvrc_mean_path = '/home/arka_s/Caffe/caffe/python/caffe/imagenet/ilsvrc_2012_mean.npy'
     net = caffe.Net(descriptor_path, weights_path, caffe.TEST)
@@ -29,9 +29,21 @@ if __name__ == '__main__':
 
     img1_tdir = '../../data/protest_data/only_text/beach_mdf_019/'
     img2_tdir = '../../data/protest_data/only_text/protest_img_019/'
-    # for i in os.listdir(img1_tdir)[:1]:
-    # for j in os.listdir(img2_tdir)[:1]:
-    #     a1 = two_imgs_matcher.are_the_two_imgs_same(img1_tdir + i,
-    #                                                 img2_tdir + j, transformer, net)
-    a1 = two_imgs_matcher.are_the_two_imgs_same(img1_tdir + '0.png',
-                                                img2_tdir + '1.png', transformer, net)
+    mcor = 0
+    max_id = [0, 0]
+    itern = 0
+    for i in os.listdir(img1_tdir)[:]:
+        for j in os.listdir(img2_tdir)[:]:
+            itern += 1
+            a1 = two_imgs_matcher.are_the_two_imgs_same(img1_tdir + i,
+                                                        img2_tdir + j, transformer, net)
+            corr = a1[5]['pear_ncc']
+
+            if corr > mcor:
+                mcor = corr
+                max_id[0] = i
+                max_id[1] = j
+
+            print ('Itern ', itern)
+    # a1 = two_imgs_matcher.are_the_two_imgs_same(img1_tdir + '0.png',
+    # img2_tdir + '1.png', transformer, net)
